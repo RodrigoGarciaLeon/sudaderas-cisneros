a3bcffe5d62f57c64deeb23ed54fc2b22b5286e6
RodrigoGarciaLeon <rodrigarl46@gmail.com>
2023-10-05 19:18:51 +0200

    modals actualizados

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -2,6 +2,10 @@ const overlay1 = document.getElementById("overlay-1");
 const overlay2 = document.getElementById("overlay-2");
 const overlay3 = document.getElementById("overlay-3");
 const overlay4 = document.getElementById("overlay-4");
+const overlay_votaciones = document.getElementById("votaciones");
+const btn_envio = document.getElementById("envio");
+const modal_envio = document.getElementById("modal-envio");
+const btn_cerrar_envio = document.getElementById("close-envio");
 
 const sudaderaBlanca = document.getElementById("sudadera-blanca");
 const sudaderaAzul = document.getElementById("sudadera-azul");
@@ -17,6 +21,9 @@ overlay1.addEventListener("click", close_overlay);
 overlay2.addEventListener("click", close_overlay);
 overlay3.addEventListener("click", close_overlay);
 overlay4.addEventListener("click", close_overlay);
+overlay_votaciones.addEventListener("click", close_votaciones);
+btn_envio.addEventListener("click", open_envio);
+btn_cerrar_envio.addEventListener("click", close_envio);
 
 function sudaderaBlanca_f() {
   overlay1.style.display = "flex";
@@ -32,9 +39,21 @@ function polarVerde_f() {
   overlay4.style.display = "flex";
 }
 
+function open_envio() {
+  modal_envio.style.display = "flex";
+}
+
 function close_overlay() {
   overlay1.style.display = "none";
   overlay2.style.display = "none";
   overlay3.style.display = "none";
   overlay4.style.display = "none";
+}
+
+function close_votaciones() {
+  overlay_votaciones.style.display = "none";
+}
+
+function close_envio() {
+  modal_envio.style.display = "none";
 }
