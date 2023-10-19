4e6bb1a08da409e2a387e0c05f713b570de567ac
RodrigoGarciaLeon <rodrigarl46@gmail.com>
2023-10-19 18:19:13 +0200

    popup votaciones quitado

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -2,7 +2,7 @@ const overlay1 = document.getElementById("overlay-1");
 const overlay2 = document.getElementById("overlay-2");
 const overlay3 = document.getElementById("overlay-3");
 const overlay4 = document.getElementById("overlay-4");
-const overlay_votaciones = document.getElementById("votaciones");
+// const overlay_votaciones = document.getElementById("votaciones");
 const btn_envio = document.getElementById("envio");
 const modal_envio = document.getElementById("modal-envio");
 const btn_cerrar_envio = document.getElementById("close-envio");
@@ -21,7 +21,7 @@ overlay1.addEventListener("click", close_overlay);
 overlay2.addEventListener("click", close_overlay);
 overlay3.addEventListener("click", close_overlay);
 overlay4.addEventListener("click", close_overlay);
-overlay_votaciones.addEventListener("click", close_votaciones);
+// overlay_votaciones.addEventListener("click", close_votaciones);
 btn_envio.addEventListener("click", open_envio);
 btn_cerrar_envio.addEventListener("click", close_envio);
 
@@ -56,10 +56,10 @@ function close_overlay() {
   document.body.style.overflow = "auto";
 }
 
-function close_votaciones() {
-  overlay_votaciones.style.display = "none";
-  document.body.style.overflow = "auto";
-}
+// function close_votaciones() {
+//   overlay_votaciones.style.display = "none";
+//   document.body.style.overflow = "auto";
+// }
 
 function close_envio() {
   modal_envio.style.display = "none";
